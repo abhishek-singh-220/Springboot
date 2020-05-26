927ea98a92a4eabde9153a9cc6239458fd7c077e
abhishek-singh-220 <abhishekrajput220@gmail.com>
2020-05-26 22:30:19 +0530

    Update containercheck.sh

--- complete/containercheck.sh
+++ complete/containercheck.sh
@@ -1,10 +1,14 @@
 count=$(docker ps -a | grep springbootapp | wc -l)
 if (($count>0))
 then
-echo '$count container is running'
+echo '($count) container is running'
 docker stop springbootapp
 docker rm springbootapp
+echo 'springbootapp is stopeed and removed'
+docker run -d -p 9098:8080 --name springbootapp springbootapp
+echo 'springbootapp container is started'
 else
 echo 'No Container is running'
 docker run -d -p 9098:8080 --name springbootapp springbootapp
+echo 'springbootapp container is started'
 fi
